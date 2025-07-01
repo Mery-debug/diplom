ace6777246970627260c25fe142ea3a7174a3bcb
Мария Шапошникова <m.a.shap58000@gmail.com>
2025-07-01 03:22:56 +0300

    first

--- task_tg/models.py
+++ task_tg/models.py
@@ -1,5 +1,5 @@
 from six import integer_types
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 from task_tg.database import Base
 
@@ -8,18 +8,24 @@ class Tags(Base):
     __tablename__ = 'tags'
 
     id = Column(Integer, primary_key=True)
+    tags = Column(String, unique=True)
 
 
+class Rating(Base):
+    __tablename__ = 'rating'
+
+    id = Column(Integer, primary_key=True)
+    rating = Column(Integer)
 
 class Problems(Base):
     __tablename__= 'problems'
 
     id = Column(Integer, primary_key=True)
-    tags = Column()
-    name
-    contestId
-    index
-    type_problems
-    rating
+    tags = Column(String, ForeignKey('tags.id'))
+    name = Column(String)
+    contestId = Column(Integer)
+    index = Column(String)
+    type_problems = Column(String)
+    rating = Column(Integer, ForeignKey('rating.id'))
 
 
